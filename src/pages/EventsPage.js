3ad715c3536407584bfa2178ce070bc13df694b6
Christian Watson <chris23w@gmail.com>
2019-07-27 22:42:24 -0700

    fixed merge conflict errors, again

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -24,7 +24,6 @@ class EventsPage extends Component {
             aria-controls="collapseOne">
             + push me to toggle event details
           </button>
-       
         </div>
         <div
           id={"e" + idx}
