520aab090ec06d25de3c0dcf8b0f0c2fa71871b7
alison-codes <alisonnorris314@gmail.com>
2019-07-28 16:55:24 -0700

    Test Matchy card one mobile web

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ConfettiGenerator from "confetti-js";
 
-import interest1 from '../images/Gaming.png';
+import dog from '../images/dog.svg';
+import computer from '../images/computer.svg';
 
 
 const LandingPage = (props) => {
     React.useEffect(() => {
         const confettiSettings = { target: 'my-canvas' };
         const confetti = new ConfettiGenerator(confettiSettings);
-        
         confetti.render();
         setTimeout(confetti.clear, 100000);
         return () => confetti.clear();
@@ -17,7 +17,12 @@ const LandingPage = (props) => {
 
     return (
         <div className="landing-background">
-            Landing page
+            <main id="color-background1">
+                <h1 id="matchy-greeting1">HOLA!</h1>
+                <img id="matchy-image1" className="interstImg" src={dog} alt="User interest" />
+                <h1 id="matchy-number-msg1">LOOK FOR 5 OTHER FOLKS WITH THE SAME CARD</h1>
+            </main>
+
             <section>
                 <Link to='/events' className='NavBar-link'>Get Started</Link>
             </section>
@@ -30,3 +35,12 @@ const LandingPage = (props) => {
 
 export default LandingPage;
 
+
+
+
+
+{/* <main id="color-background2">
+    <h1 id="matchy-greeting2">HEY!</h1>
+    <img id="matchy-image2" className="interstImg" src={computer} alt="User interest" />
+    <h1 id="matchy-number-msg2">LOOK FOR 2 OTHER FOLKS WITH THE SAME CARD</h1>
+</main> */}
